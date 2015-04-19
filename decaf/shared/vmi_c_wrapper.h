54bb7352844dfe8a20d2797a82712130fae22fea
chengchengXCC <cxu100@syr.edu>
2015-04-19 14:06:49 -0400

    Update vmi_c_wrapper.h

--- decaf/shared/vmi_c_wrapper.h
+++ decaf/shared/vmi_c_wrapper.h
@@ -17,23 +17,23 @@ extern "C" {
 typedef struct _tmodinfo
 {
 	char	    name[512]; ///< module name
-	/*uint32_t*/target_ulong	base;  ///< module base address
-	/*uint32_t*/ target_ulong	size;  ///< module size
-}tmodinfo_t;
+	uint32_t/*target_ulong*/	base;  ///< module base address
+	uint32_t/*target_ulong*/	size;  ///< module size
+ }tmodinfo_t;
 
 typedef struct _old_tmodinfo
 {
   char name[32];
-  /*uint32_t*/target_ulong base;
- /* uint32_t*/target_ulong size;
+  uint32_t/*target_ulong*/ base;
+  uint32_t/*target_ulong*/ size;
 }old_modinfo_t;
 
 typedef struct _procinfo
 {
- /* uint32_t*/target_ulong pid;
- /* uint32_t*/target_ulong cr3;
-  size_t n_mods;
-  char name[512];
+  uint32_t/*target_ulong*/ pid;
+  uint32_t/*target_ulong*/ cr3;
+   size_t n_mods;
+   char name[512];
 }procinfo_t;
 
 
@@ -48,14 +48,14 @@ extern int   VMI_locate_module_c(gva_t eip, gva_t cr3, char proc[],tmodinfo_t *t
 //extern int checkcr3(uint32_t cr3, uint32_t eip, uint32_t tracepid, char *name,
   //           int len, uint32_t * offset);
 
-extern int VMI_locate_module_byname_c(const char *name, /*uint32_t*/target_ulong pid,tmodinfo_t * tm);
+extern int VMI_locate_module_byname_c(const char *name,uint32_t/*target_ulong*/ pid,tmodinfo_t * tm);
 
 
-extern int VMI_find_cr3_by_pid_c(/*uint32_t*/target_ulong pid);
+extern int VMI_find_cr3_by_pid_c(uint32_t/*target_ulong*/ pid);
 
-extern int VMI_find_pid_by_cr3_c(/*uint32_t*/target_ulong cr3);
+ extern int VMI_find_pid_by_cr3_c(uint32_t/*target_ulong*/ cr3);
 
-extern int VMI_find_pid_by_name_c(const char* proc_name);
+ extern int VMI_find_pid_by_name_c(const char* proc_name);
 
 /// @ingroup semantics
 /// find process given a memory space id
@@ -63,11 +63,11 @@ extern int VMI_find_pid_by_name_c(const char* proc_name);
 /// @param proc process name (output argument)
 /// @param pid  process pid (output argument)
 /// @return number of modules in this process
-extern int VMI_find_process_by_cr3_c(/*uint32_t*/target_ulong cr3, char proc_name[], size_t len, /*uint32_t*/target_ulong *pid);
+extern int VMI_find_process_by_cr3_c(uint32_t/*target_ulong*/ cr3, char proc_name[], size_t len, uint32_t/*target_ulong*/ *pid);
 /* find process name and CR3 using the PID as search key  */
-extern int VMI_find_process_by_pid_c(/*uint32_t*/target_ulong pid, char proc_name[], size_t len, /*uint32_t*/target_ulong *cr3);
+extern int VMI_find_process_by_pid_c(uint32_t/*target_ulong*/ pid, char proc_name[], size_t len, uint32_t/*target_ulong*/ *cr3);
 
-extern int VMI_get_proc_modules_c(/*uint32_t*/target_ulong pid, /*uint32_t*/target_ulong mod_no, tmodinfo_t *buf);
+extern int VMI_get_proc_modules_c(uint32_t/*target_ulong*/ pid, uint32_t/*target_ulong*/ mod_no, tmodinfo_t *buf);
 
 extern int VMI_get_all_processes_count_c(void);
 /* Create array with info about all processes running in system
@@ -76,7 +76,7 @@ extern int VMI_find_all_processes_info_c(size_t num_proc, procinfo_t *arr);
 
 
 //Aravind - added to get the number of loaded modules for the process. This is needed to create the memory required by get_proc_modules
-extern int VMI_get_loaded_modules_count_c(/*uint32_t*/target_ulong pid);
+extern int VMI_get_loaded_modules_count_c(uint32_t/*target_ulong*/ pid);
 //end - Aravind
 
 
@@ -92,7 +92,7 @@ extern int VMI_get_current_tid_c(CPUState* env);
 extern int VMI_get_guest_version_c(void);
 
 
-extern int VMI_list_modules(Monitor *mon, /*uint32_t*/target_ulong pid);
+extern int VMI_list_modules(Monitor *mon, uint32_t/*target_ulong*/ pid);
 extern int VMI_list_processes(Monitor *mon);
 
 #ifdef __cplusplus
